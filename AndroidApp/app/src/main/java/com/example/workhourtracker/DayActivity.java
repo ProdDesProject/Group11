2b30cf1ee70c3efa4151821b696656f3a12f72b6
are98 <arttu.kemppainen@outlook.com>
2020-11-27 13:30:24 +0200

    signUp fully workin

--- AndroidApp/app/src/main/java/com/example/workhourtracker/DayActivity.java
+++ AndroidApp/app/src/main/java/com/example/workhourtracker/DayActivity.java
@@ -2,13 +2,21 @@ package com.example.workhourtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class DayActivity extends AppCompatActivity {
+
+    String selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
+
+        selectedDate = "DD.MM.YYYY";
+        Intent myIntent = getIntent();
+        selectedDate = myIntent.getExtras().getString("choosedDay");
+
     }
 }
