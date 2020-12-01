2d691d8efc8edf5adf551245df56dded49cbc340
TeemuRov <t8rote00@students.oamk.fi>
2020-12-01 13:49:59 +0200

    added listview to dayActivity

--- AndroidApp/app/src/main/java/com/example/workhourtracker/DayActivity.java
+++ AndroidApp/app/src/main/java/com/example/workhourtracker/DayActivity.java
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 
 public class DayActivity extends AppCompatActivity {
+
+    private TextView day;
+    private ListView activitiesList;
+    private final int count = 12;
+    String[] activity_list = {"8:00 - 16:00", "8:01 - 20:02", "9:20 - 16:05"};
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,20 @@ public class DayActivity extends AppCompatActivity {
 
         String date = intent.getStringExtra("Date");
         String id = intent.getStringExtra("userId");
+
+        day = findViewById(R.id.calendarDate);
+        day.setText(date);
+
+
+        listView = (ListView) findViewById(R.id.list_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.added_hours,R.id.hours,activity_list);
+        listView.setAdapter(adapter);
+
+
+
+
+
+
     }
+
 }
